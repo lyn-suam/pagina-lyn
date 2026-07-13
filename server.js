679621389e8276cbdd90f13f057bcf2eeda73c2b
lyn-suam <esuam30@gmail.com>
2026-07-12 19:16:00 -0500

    Conectar imágenes de forma permanente con Cloudinary

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
-const mongoose = require('mongoose'); // Usamos Mongoose para MongoDB
+const mongoose = require('mongoose');
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 const app = express();
 
-// Configuración de almacenamiento para imágenes
-const storage = multer.diskStorage({
-    destination: './uploads/',
-    filename: (req, file, cb) => cb(null, Date.now() + path.extname(file.originalname))
+// CONFIGURACIÓN DE CLOUDINARY (Pon tus datos aquí)
+cloudinary.config({ 
+  cloud_name: 'pzgr0js', 
+  api_key: '336281365133553', 
+  api_secret: 'qs2Fano3P1BSu1B5ThOC1-0Re9Y' // <-- Pega aquí lo que sale al dar clic en Revelar
+});
+
+// Configurar Multer para que envíe los archivos directo a Cloudinary
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'tienda_productos', // Nombre de la carpeta que se creará en Cloudinary
+    allowed_formats: ['jpg', 'png', 'jpeg', 'webp']
+  },
 });
 const upload = multer({ storage });
 
@@ -27,10 +38,11 @@ const Producto = mongoose.model('Producto', {
 });
 
 // Middlewares
+/*
 app.use(express.json());
 app.use(express.static('.')); 
 app.use('/uploads', express.static('uploads'));
-
+*/
 // --- RUTAS DE LA API ---
 
 // 1. OBTENER PRODUCTOS (MongoDB)
@@ -44,16 +56,18 @@ app.get('/productos', async (req, res) => {
 });
 
 // 2. CREAR PRODUCTO (MongoDB)
+// CREAR PRODUCTO (Subiendo imagen a Cloudinary de forma permanente)
 app.post('/productos', upload.single('imagen'), async (req, res) => {
     try {
         const nuevoProducto = new Producto({
             nombre: req.body.nombre,
             precio: req.body.precio,
-            imagen: '/uploads/' + req.file.filename
+            imagen: req.file.path // <-- Cloudinary nos da la URL directa aquí automáticamente
         });
         await nuevoProducto.save();
-        res.send("Producto guardado en la nube");
+        res.send("Producto guardado en la nube de forma permanente");
     } catch (err) {
+        console.error(err);
         res.status(500).send("Error al guardar el producto");
     }
 });
